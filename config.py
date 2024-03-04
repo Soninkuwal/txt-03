36055c8aa11f7406077d101292e81d6f5e8bfe1b
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-03-04 09:29:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = 25919081
+API_ID = API_ID = 27803618
 
-API_HASH = os.environ.get("API_HASH", "0bc2fdba14b16b44f0d89729ed8d2118")
+API_HASH = os.environ.get("API_HASH", "2cdaef9643189f6bd9c7b31a70257356")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6911744817:AAFH-5aOTr9hrR-VYmHFKK4w_1nXsbQ-vqw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7044834090:AAHoMqoyCdgQUHVuKDpUiUO-_BUlZla8bW8")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 1826828317))
+OWNER = int(os.environ.get("OWNER", 745084259))
 
-LOG = -1002015954185
+LOG = -1002030499286
 
